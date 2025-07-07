df0713f1d99af2e972279ad1566934e2777e88a8
Palak Mishra <160209841+palakmishra32@users.noreply.github.com>
2025-07-07 15:33:46 +0530

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -150,4 +150,5 @@ def decode_api():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render provides the port
+    app.run(host='0.0.0.0', port=port)
